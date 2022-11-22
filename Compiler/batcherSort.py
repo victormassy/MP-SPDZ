992728d5b202883c9c0170ae3a1f3d4e493a0008
Ubuntu <azureuser@first-helper.tv13t4qjrccelh53cpia2lkyub.parx.internal.cloudapp.net>
2022-11-22 13:50:34 +0000

    Batcher sort for matrix

--- Compiler/batcherSort.py
+++ Compiler/batcherSort.py
@@ -1,5 +1,22 @@
-def batcherSort(a, sorted_length=1, n_parallel=32,
-                              n_threads=None):
+from Compiler.library import *
+from Compiler.types import *
+
+
+def output_swap(base, step,swapHappened, output):
+    #print_ln('swap %s %s %s %s %s %s ',swapHappened.reveal(), base, base+step.reveal(), base * swapHappened.reveal(),(base+step.reveal()) * swapHappened.reveal(),(base+step.reveal()) * swapHappened.reveal())
+    #swapHappened = swapHappened.if_else(0,1)
+    temp = output[base * swapHappened.reveal()]
+    output[base * swapHappened.reveal()] = output[(base+step.reveal()) * swapHappened.reveal()]
+    output[(base+step.reveal()) * swapHappened.reveal()] = temp
+    
+    #output.print_reveal_nested()
+
+def batcher_sort(a, sorted_length=1, n_parallel=32,
+        n_threads=None):
+    output = Array(len(a), sint)
+    for i in range(len(a)):
+            output[i] = i
+    
     steps = {}
     l = sorted_length
     while l < len(a):
@@ -23,23 +40,57 @@ def batcherSort(a, sorted_length=1, n_parallel=32,
                         step = l//k
                         def swap(base, step):
                             if m == len(a):
+                                x = a[base]
+                                tmp = cond_swap(a[base], a[base + step])
                                 a[base], a[base + step] = \
-                                    cond_swap(a[base], a[base + step])
+                                    tmp
+                                return x != tmp[0]
                             else:
+                                #temp = a[base]
                                 # ignore values outside range
                                 go = base + step < len(a)
                                 x = a.maybe_get(go, base)
                                 y = a.maybe_get(go, base + step)
                                 tmp = cond_swap(x, y)
                                 for i, idx in enumerate((base, base + step)):
-                                    a.maybe_set(go, idx, tmp[i])
+                                    a.maybe_set(go, idx, tmp[i]) 
+                                return x != tmp[0]
                         if k == 2:
-                            swap(base, step)
+                            #newidx = (base < len(a)).if_else(base, 0)
+                            #temp = a[newidx] 
+                            swap_happened = swap(base, step)
+                            #swapHappened = (a[newidx] != temp)
+                            #print_ln('newidx %s', newidx.reveal())
+                            #print_ln('swap happened %s', swapHappened.reveal())
+                            output_swap(base, step, swap_happened, output)
+                            #a.print_reveal_nested()
                         else:
                             @for_range_opt(n_innermost)
                             def f(i):
                                 m1 = step + i * 2 * step
                                 m2 = m1 + base
-                                swap(m2, step)
+                                #newidx = (m2 < len(a)).if_else(m2, 0)
+                                #temp = a[newidx]
+                                swap_happened = swap(m2, step)
+                                #swapHappened = (a[newidx] != temp)
+                                #print_ln('newidx %s', newidx.reveal())
+                                #print_ln('swap happened %s', swapHappened.reveal())
+                                output_swap(m2, step, swap_happened, output)
+                                #a.print_reveal_nested()
                 steps[key] = step
             steps[key](l)
+    return output
+
+
+
+def batcher_sort_matrix(matrix,match_keys):
+    permutation = batcher_sort(match_keys) 
+    res = Matrix(len(match_keys), len(matrix[0]), sint)
+    permutation.print_reveal_nested()
+    for i in range(len(match_keys)):
+        for j in range(len(matrix[0])):
+            #print_ln('%s', permutation.reveal()[i])
+            res[i][j] = matrix[permutation.reveal()[i]][j]
+    return res
+        
+
