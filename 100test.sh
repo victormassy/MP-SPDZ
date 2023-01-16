2f69b75f299d350947969db429c20c1f89075649
Ubuntu <azureuser@first-helper.tv13t4qjrccelh53cpia2lkyub.parx.internal.cloudapp.net>
2023-01-16 13:50:48 +0000

    automation v2

--- 100test.sh
+++ 100test.sh
@@ -1,3 +1,6 @@
+./compile.py -R 32 ipae2e
 for i in {1..10} ; do
-       ./replicated-ring-party.x --player 0 ipae2e -ip Networking/coordination
+	./replicated-ring-party.x --player 0 ipae2e -ip Networking/coordination &> results.txt
+	#grep 'Time' results.txt >> times.txt 
+	#grep 'Data sent' results.txt >> data.txt
 done
